f61ce228eb5ef6004f72b8eea9cc492d2e424fce
Rafael <110244607+RafaelG09@users.noreply.github.com>
2024-07-04 08:36:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,65 +9,50 @@ const perguntas = [
         enunciado: "Analise os impactos das reformas educacionais recentes, como o Novo Ensino Médio, na qualidade da educação e no desenvolvimento de competências necessárias para os desafios contemporâneos, considerando aspectos como flexibilização curricular, formação integral dos estudantes e preparação para o mercado de trabalho.",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Qual o impacto?",
+                afirmacao: "O Novo Ensino Médio tem 1.000 horas anuais, visando à expansão gradual para 1.400 horas, conforme o Plano Nacional de Educação. "
             },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
-            }
+           
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "O novo Ensino Médio propõe uma participação muito mais ativa do aluno, tornando-o protagonista em sua aprendizagem e na construção de seu projeto de vida.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Quais são as principais mudanças trazidas pela reforma do ensino médio?",
+                
             },
-            {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
-            }
+           
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "A carga horária do Ensino Médio passou de 800 para 1.000 horas atuais, mas passaram  a ser pelo menos 3.000 horas para três últimos anos da Educação Básica. O ano letivo passou a durar 200 dias. Para cumprir a carga horária total, os estudantes do Ensino Médio terão que dedicar mais horas ao ensino escolar.",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Qual a crítica ao Novo Ensino Médio?",
+                
             },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
-            }
+           
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Segundo a pesquisa, 55% dos alunos se mostraram insatisfeitos com as mudanças decorrentes do NEM. Outros 63% criticaram a mudança na carga horária. Entre os professores, 76% se mostraram insatisfeitos com NEM. Já entre os gestores, esse índice é de 66%.",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "O que os jovens acham do Novo Ensino Médio?",
+                
             },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
+           
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Os estudantes que estão cursando o novo modelo de Ensino Médio o avaliam como positivo, se dizem mais satisfeitos com a escola e otimistas com o futuro profissional. 61% dos alunos que estão cursando o Novo Ensino Médio avaliam positivamente o modelo. ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Resumo do que foi dito sobre o Novo Ensino Médio!",
+                afirmacao: "Disciplinas obrigatórias incluem língua portuguesa, língua inglesa, língua espanhola, arte, educação física, matemática, história, geografia, sociologia, filosofia, física, química e biologia."
             },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
+           
         ]
     },
 ];
@@ -105,7 +90,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Em 2022...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
